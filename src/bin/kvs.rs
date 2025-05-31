39abeffbb7f6ef3483d5a9dfccf4cbc23cdc7bc5
Ban Nguy <ban.nguy@tutanota.com>
2025-05-31 18:37:54 +0700

    feat: accept command options according to tests

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,27 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version = "0.1.0")]
+#[command(about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Get { key: String },
+    Set { key: String, value: String },
+    Rm { key: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Some(Commands::Get { key: _key }) => { panic!("unimplemented") },
+        Some(Commands::Set { key: _key, value: _value }) => { panic!("unimplemented") },
+        Some(Commands::Rm { key: _key }) => { panic!("unimplemented") },
+        None => { println!("no subcommand given") },
+    }
 }
